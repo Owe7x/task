76fe8b58e9df3cc140701d1dab9455fc05c2de5a
Tim Rakhmanov <owe7x@yandex.ru>
2025-08-02 12:48:55 +0300

    task 1

--- main.go
+++ main.go
@@ -1,22 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
-func runGoroutine() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done()
-		fmt.Println("Hello from goroutine!")
-	}()
-
-	wg.Wait()
-}
-
-func main() {
-	runGoroutine()
-}
+import ()
