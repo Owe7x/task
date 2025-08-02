3da0f21b0c8d55f3b6d4d65db304fc93a07e17c3
Тимур <owe7x@yandex.ru>
2025-08-02 21:30:24 +0300

    All Task

--- NewTask/task10.go
+++ NewTask/task10.go
@@ -1,18 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func main() {
 	ch1 := make(chan int)
-	ch2 := make(chan int)
+	const workerPool = 10
+	var wg sync.WaitGroup
 
 	go func() {
-		for i := 0; i < 5; i++ {
+		for i := 0; i < 50; i++ {
 			ch1 <- i
 		}
-		for i := 5; i < 10; i++ {
-			ch2 <- i
-		}
 		close(ch1)
-		close(ch2)
 	}()
 
+	outs := fanOut(ch1, workerPool)
+
+	wg.Add(len(outs))
+
+	for i, out := range outs {
+		go func(id int, c <-chan int) {
+			defer wg.Done()
+			for v := range c {
+				fmt.Printf("worker %d output: %d\n", id+1, v)
+			}
+		}(i, out)
+	}
+
+	wg.Wait()
+}
+
+func fanOut(ch1 <-chan int, workerPool int) []<-chan int {
+	outs := make([]<-chan int, workerPool)
+	for i := 0; i < workerPool; i++ {
+		outs[i] = pipeline(ch1)
+	}
+	return outs
+}
+
+func pipeline(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for v := range in {
+			out <- v * 2
+		}
+	}()
+	return out
 }
