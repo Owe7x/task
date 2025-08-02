693b0fbcf7a5e7fa5b56c1464413031d31de8ab0
Tim Rakhmanov <owe7x@yandex.ru>
2025-08-02 13:10:32 +0300

    Задача 2

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	const n = 5
+
+	for i := 1; i <= n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println(i)
+		}()
+	}
+
+	wg.Wait()
+}
